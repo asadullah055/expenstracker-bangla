bc8dd8fc90159b9e23938b93bd57399c3d11ae96
asadullah055 <asadullahahmed01@gmail.com>
2026-02-26 09:43:17 +0600

    add api

--- src/utils/apiPaths.js
+++ src/utils/apiPaths.js
@@ -1,6 +1,6 @@
-export const BASE_URL = "http://localhost:8000";
 // utils/apiPaths.js
-
+// export const BASE_URL = "http://localhost:8000";
+export const BASE_URL = "https://expensetracker-bangla-server.vercel.app";
 export const API_PATHS = {
   AUTH: {
     LOGIN: "/api/v1/auth/login",
